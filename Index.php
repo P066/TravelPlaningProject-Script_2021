0667735e2ee2536b5e235373555e6469c8b5a390
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-02-02 00:06:29 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -3,14 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Database</title>
+    <title>Travel Plan System - 2021</title>
 </head>
 <body>
-<<<<<<< HEAD
 
-=======
     <p>กุ๊กๆ ไก่น้อย</p>
     
->>>>>>> a7f0758fb94ac7579df9b155050c2de8dc87c6fd
 </body>
 </html>
