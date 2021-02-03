1fdb682b620af8449a29a2d4bd399521950be0ae
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-02-04 01:11:45 +0700

    Commit 04/02/2564 01.11

---
+++ Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Plan System</title>
+    <?php include 'cdn.htm';?>
+</head>
+<body>
+    <div class="container my-3">
+        <div class="header">
+        	Test
+        <div class="body">
+            Test
+        </div>
+
+        <div class="footer">
+            Test
+        </div>
+    </div>
+    
+</body>
+</html>
